6e91892577a383366b419c4dd4cf181b6c7498e4
Kazare <12612446+kazare@users.noreply.github.com>
2020-12-30 15:02:51 -0500

    Add drop down menu wip

--- src/Filters.js
+++ src/Filters.js
@@ -1,10 +1,40 @@
+import React, { useState } from "react";
 import "./App";
+import "./Filters.css";
 
 const Filters = ({ sort, activeBtn, onChange }) => {
+   const [active, setActive] = useState(false);
+
    const handleClick = (event, name) => {
       event.preventDefault();
 
       onChange(event.target.value, name);
+   };
+
+   const DropDown = () => {
+      return (
+         <div className="dropDown">
+
+
+            <button>Card</button>
+            <button>Classic</button>
+            <button>Compact</button>
+
+
+         </div>
+      );
+   };
+
+   const DD2 = () => {
+      return (
+         <div className="customS">
+            <select >
+               <option value="1">Card</option>
+               <option value="2">Classic</option>
+               <option value="3">Compact</option>
+            </select>
+         </div>
+      );
    };
 
    return (
@@ -27,12 +57,12 @@ const Filters = ({ sort, activeBtn, onChange }) => {
             onClick={(e) => { handleClick(e, 'top') }}>
             TOP
                </button>
-         <button className="cardBtn">Card</button>
-         <div className="dropDown">
-            <button>Card</button>
-            <button>Classic</button>
-            <button>Compact</button>
-         </div>
+
+
+         <button className="cardBtn" onClick={(e) => { !active ? setActive(true) : setActive(false) }} >Card</button>
+         {!active ? <DropDown /> : null}
+
+         <DD2 />
       </div>
    );
 };
